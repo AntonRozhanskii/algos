798a18395ae1f592a75ce1f25aefa8e792b5320e
Anton <anton_rozhanskii@yahoo.com>
2023-12-02 00:40:42 +0200

    Added leading zero into names

---
+++ algos-0020-valid-parentheses/src/main/java/org/example/MySolutionTwo.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.util.Stack;
+
+public class MySolutionTwo {
+
+    public boolean isValid(String s) {
+        if (s.length() % 2 == 1) {
+            return false;
+        }
+        Stack<Character> buffer = new Stack<>();
+        for (Character c : s.toCharArray()) {
+            switch (c) {
+                case '}':
+                case ')':
+                case ']':
+                    if (!isValidWithBuffer(c, buffer)) return false;
+                    break;
+                default:
+                    buffer.push(c);
+            }
+        }
+        return buffer.isEmpty();
+    }
+
+    private boolean isValidWithBuffer(char underTest, Stack<Character> buffer){
+        if (buffer.isEmpty()) {
+            return false;
+        }
+        switch (underTest) {
+            case '}':
+                if (!buffer.pop().equals('{')) return false;
+                break;
+            case ']':
+                if (!buffer.pop().equals('[')) return false;
+                break;
+            case ')':
+                if (!buffer.pop().equals('(')) return false;
+                break;
+        }
+        return true;
+    }
+}
